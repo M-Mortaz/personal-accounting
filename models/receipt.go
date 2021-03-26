2679d1c8bf7549046c1876b6476ac573020660d1
Mehdi <MehdiMortaz@gmail.com>
2021-03-27 02:11:13 +0430

    added more fields to the receipt model

--- models/receipt.go
+++ models/receipt.go
@@ -1,9 +1,19 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type Receipt struct {
 	gorm.Model
+	Value int
+	Description string
+	HappenAt time.Time
+	Hidden bool
+	Type string
 	UserID uint
 	TagID uint
+	SourceID uint
+	SubCategoryID uint
 }
